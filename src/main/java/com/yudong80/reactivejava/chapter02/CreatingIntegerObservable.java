fba11d029083a0523ca56ad3ef7c439c7cd23261
yudong80 <koreacio@gmail.com>
2016-12-27 05:21:06 +0900

    Added CreatingIntegerObservable, CretatingOnlyObservable, SubscribeObservable class

---
+++ src/main/java/com/yudong80/reactivejava/chapter02/CreatingIntegerObservable.java
@@ -0,0 +1,22 @@
+package com.yudong80.reactivejava.chapter02;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class CreatingIntegerObservable {
+	public void run() { 
+		Observable<Integer> source = 
+		    Observable.create((ObservableEmitter<Integer> emitter) -> { 
+		    	emitter.onNext(100);
+		    	emitter.onNext(200);
+		    	emitter.onNext(300);
+		    	emitter.onComplete();
+		    }); 
+		source.subscribe(System.out::println);
+	}
+	
+	
+	public static void main(String[] args){ 
+		new CreatingIntegerObservable().run();
+	}
+}
---
+++ src/main/java/com/yudong80/reactivejava/chapter02/CreatingOnlyObservable.java
@@ -0,0 +1,20 @@
+package com.yudong80.reactivejava.chapter02;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class CreatingOnlyObservable {
+	public void run() { 
+		Observable<Integer> source = 
+		    Observable.create((ObservableEmitter<Integer> emitter) -> { 
+		    	emitter.onNext(100);
+		    	emitter.onNext(200);
+		    	emitter.onNext(300);
+		    	emitter.onComplete();
+		    }); 
+	}
+	
+	public static void main(String[] args){ 
+		new CreatingOnlyObservable().run();
+	}
+}
