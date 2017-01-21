f19ead1e8a7f162535deab3b2041e354a8922bd6
yudong80 <koreacio@gmail.com>
2017-01-22 02:11:59 +0900

    Fixed SchedulerBasic class

--- src/main/java/com/yudong80/reactivejava/chapter05/SchedulersBasic.java
+++ src/main/java/com/yudong80/reactivejava/chapter05/SchedulersBasic.java
@@ -1,11 +1,14 @@
 package com.yudong80.reactivejava.chapter05;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import com.yudong80.reactivejava.common.CommonUtils;
 
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class SchedulersBasic {
@@ -35,14 +38,40 @@ public class SchedulersBasic {
 		CommonUtils.exampleComplete();
 	}
 	
-	public void intervalWithComputationScheduler() { 
-		Observable<Long> source1 = Observable.interval(100L, TimeUnit.MILLISECONDS)
-				.map(i -> i * 100);
+	public void usingInterval() { 
+		Observable<Long> source1 = Observable.interval(100L, TimeUnit.MILLISECONDS);
 		Observable<String> source2 = Observable.interval(200L, TimeUnit.MILLISECONDS)
 				.map(CommonUtils::numberToAlphabet);				
-		source1.subscribe(CommonUtils::logWithThread);
-		source2.subscribe(CommonUtils::logWithThread);
+		Disposable sub1 = source1.subscribe(CommonUtils::logWithThread);
+		Disposable sub2 = source2.subscribe(CommonUtils::logWithThread);
 		CommonUtils.sleep(500);
+		sub1.dispose();
+		sub2.dispose();		
+		CommonUtils.exampleComplete();
+	}
+	
+	public void usingIOScheduler() { 
+		String root = "c:\\";
+		Observable<String> source = Observable.fromArray(new File(root).listFiles())
+				.map(f -> f.getAbsolutePath());
+		
+		Disposable sub = source.subscribeOn(Schedulers.io())
+				.subscribe(CommonUtils::logWithThread);
+		CommonUtils.sleep(500);
+		sub.dispose();
+		CommonUtils.exampleComplete();
+	}
+	
+	public void usingNewThreadScheduler() { 
+		Observable<String> source = Observable.just("ONE", "TWO", "THREE");
+		
+		Disposable sub1 = source.subscribeOn(Schedulers.newThread())
+				.subscribe(CommonUtils::logWithThread);
+		Disposable sub2 = source.subscribeOn(Schedulers.newThread())
+				.subscribe(CommonUtils::logWithThread);
+		CommonUtils.sleep(500);		
+		sub1.dispose();
+		sub2.dispose();
 		CommonUtils.exampleComplete();
 	}
 	
@@ -50,6 +79,9 @@ public class SchedulersBasic {
 		SchedulersBasic demo = new SchedulersBasic();
 //		demo.noScheduler();
 //		demo.singleScheduler();
-		demo.multiSingleScheduler();
+//		demo.multiSingleScheduler();
+//		demo.usingInterval();
+//		demo.usingIOScheduler();
+		demo.usingNewThreadScheduler();
 	}
 }
