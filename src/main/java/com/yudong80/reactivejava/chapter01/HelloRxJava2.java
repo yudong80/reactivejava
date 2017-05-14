0df6a89e1cc7c8f58e3e569f887884c6e188a5fc
yudong80 <koreacio@gmail.com>
2017-05-14 12:21:36 +0900

    Renamed HelloRxJava2

--- src/main/java/com/yudong80/reactivejava/chapter01/HelloRxJava2.java
+++ src/main/java/com/yudong80/reactivejava/chapter01/HelloRxJava2.java
@@ -2,14 +2,14 @@ package com.yudong80.reactivejava.chapter01;
 
 import io.reactivex.Observable;
 
-public class FirstExample {	
+public class HelloRxJava2 {	
 	public void emit() {
 		Observable.just("Hello", "RxJava2!!")
 		.subscribe(System.out::println);
 	}
 
 	public static void main(String args[]) { 
-		FirstExample demo = new FirstExample();
+		HelloRxJava2 demo = new HelloRxJava2();
 		demo.emit();
 	}	
 }
