ae99f8ec1542d3cc7c07537355a76dd46369aa89
yudong80 <koreacio@gmail.com>
2017-06-11 16:34:44 +0900

    Fixed comment on SingleExample.java

--- src/main/java/com/yudong80/reactivejava/chapter02/SingleExample.java
+++ src/main/java/com/yudong80/reactivejava/chapter02/SingleExample.java
@@ -14,28 +14,28 @@ public class SingleExample {
 	}
 	
 	public void fromObservable() {
-		//1. 기존 옵저버블에서 Single로 변환하기  
+		//1. 기존 Observable에서 Single 객체로 변환하기  
 		Observable<String> source = Observable.just("Hello Single");
 		Single.fromObservable(source)
 			.subscribe(System.out::println);
 		
-		//2. single() 메소드를 호출하여 Single 생성하기 
+		//2. single() 메서드를 호출해 Single 객체 생성하기 
 		Observable.just("Hello Single")
 			.single("default item")
 			.subscribe(System.out::println);
 		
-		//3. first() 메소드를 호출하여 Single 생성하기 
+		//3. first() 메서드를 호출하여 Single 객체 생성하기 
 		String[] colors = {"Red", "Blue", "Gold"};
 		Observable.fromArray(colors)
 			.first("default value")
 			.subscribe(System.out::println);
 		
-		//4. empty 옵저버블에서 Single 생성하기 
+		//4. empty Observable에서 Single 객체 생성하기 
 		Observable.empty()
 			.single("default value")
 			.subscribe(System.out::println);
 		
-		//5. take() 함수에서 Single 생성하기 
+		//5. take() 함수에서 Single 객체 생성하기 
 		Observable.just(new Order("ORD-1"), new Order("ORD-2"))
 				.take(1)
 				.single(new Order("default order"))
